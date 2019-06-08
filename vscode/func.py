b3016630857e92070f6cb25f1c0c984d22aa0235
赵锐飞 <peterchezhao@gmail.com>
2019-06-08 22:11:24 +0800

    增加函数内修改全局变量

---
+++ vscode/func.py
@@ -0,0 +1,5 @@
+def add(x, y):
+    z = x+y
+    return z
+res = add(3,5)
+print(res)
---
+++ vscode/globalVar.py
@@ -0,0 +1,8 @@
+def p_num():
+    global num
+    print(num)
+    num=20
+    print(num)
+num=10
+p_num()
+print(num)
