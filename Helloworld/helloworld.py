8e0cc55e85e6b836daa83457c18595c5ece42b08
赵锐飞 <peterchezhao@gmail.com>
2019-06-08 21:42:11 +0800

    更改文件名称

--- Helloworld/helloworld.py
+++
@@ -1,9 +0,0 @@
-import requests
- 
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-r = requests.get(url)
-print("Status code:",r.status_code)
- 
-response_dict = r.json()
-print(response_dict.keys())
-print("Total repositories:",response_dict['total_count'])
